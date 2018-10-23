874b800f7ed88941bcf366acc1cb1107642deba9
liruiqi123 <1595854626@qq.com>
2018-10-23 13:12:17 +0800

    测试创建的公共函数是否有效

--- application/user/controller/Login.php
+++ application/user/controller/Login.php
@@ -11,7 +11,9 @@ class Login
 
     public function dologin(){
          $code =  input('get.code');/*获取前端传过来的code*/
-         return($code);
+         if(!$code){
+                     ajaxReturn(['code' => 40000, 'msg'=>'code不能为空']);
+         }
 
     }
 }
