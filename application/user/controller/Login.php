f4fddea4b3d355302b66b9fe010ef2a8b9b9f3cc
liruiqi123 <1595854626@qq.com>
2018-10-23 12:59:16 +0800

    测试post方法接受参数

--- application/user/controller/Login.php
+++ application/user/controller/Login.php
@@ -8,4 +8,12 @@ class Login
         return('你好啊 北京时间10月份');
 
     }
+
+    public function dologin(){
+         $post_data = I('post.');
+         $code =  I('post.code/s');/*获取前端传过来的code*/
+         return($code);
+
+
+    }
 }
