1ad44a672c66c520a334d2fd4eea0013752e75a0
liruiqi123 <1595854626@qq.com>
2018-10-24 14:47:20 +0800

    添加开始游戏，进行答题的控制器

--- application/question/controller/Begin.php
+++ application/question/controller/Begin.php
@@ -7,4 +7,51 @@ class Begin
     {
         return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
     }
+
+
+    public function startGame() {
+            try {
+
+                $questModel = M('questions');
+                //获取题库数量
+                $questions = S('questions');
+                //$questions = null;
+                if (empty($questions)) {
+                    $questions = $questModel->where(['del'=>0])->select();
+                    S('questions', $questions, 3600);
+                }
+
+                if ($num > count($questions)) {
+                    throw_exception('题库数量不足');
+                }
+
+                $amount = count($questions);
+                //随机五个题目的id
+                $i = 0;
+                $ids = [];
+                $quests = [];
+                while ($i < $num){
+                    $rid = mt_rand(1, $amount) - 1;
+                    if (!in_array($rid, $ids)) {
+                        $ids[] = $rid;
+                        $quests[] = $questions[$rid];
+                        $i++;
+                    }
+                }
+
+                $this->ajaxReturn ( [
+                    'code' => 20000,
+                    'msg' => '获取成功',
+                    'quests' => $quests,
+                ]);
+
+            }catch (Exception $e){
+                $this->ajaxReturn ( [
+                    'code' => 40500,
+                    'msg' => '数据异常',
+                    'quests' => $quests,
+                ]);
+            }
+
+        }
 }
